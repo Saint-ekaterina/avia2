c7749b1078e94cbe8cac9494a8d8364962ddd99a
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-01-31 00:31:02 +0700

    bonus

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 15_600; //стоимость билета
+        int y = 20; // бонусные мили
+
+        System.out.println(x / y); // колличество бонусных миль от стоимости билет
+    }
+
+}
